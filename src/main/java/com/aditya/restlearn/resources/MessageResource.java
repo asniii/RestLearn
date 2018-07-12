03f6f02a407cbfca42c5653d479be273974ea6df
aditya.nehra <aditya.nehra@thermofisher.com>
2018-07-12 22:51:45 +0530

    Adding query param code

--- src/main/java/com/aditya/restlearn/resources/MessageResource.java
+++ src/main/java/com/aditya/restlearn/resources/MessageResource.java
@@ -3,11 +3,14 @@ package com.aditya.restlearn.resources;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.aditya.restlearn.model.Message;
@@ -20,13 +23,22 @@ public class MessageResource {
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Message> getMessages() {
+	public List<Message> getMessages(@QueryParam("year") int year,
+			@QueryParam("start") int start,
+			@QueryParam("size") int size) {
+		
+		if(year > 0) {
+			return messageService.getAllMesssagesForYear(year);
+		}
+		if(start>=0 && size >= 0) {
+			return messageService.getAllMessagesPaginated(start, size);
+		}
 		return messageService.getAllMessage();
 	}
 	
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON) 
 	public Message getMessage(@PathParam("messageId") long messageId) {
 		return messageService.getMessage(messageId);
 	}
@@ -36,6 +48,22 @@ public class MessageResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Message addMessage(Message message) {
 		return messageService.addMessage(message);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId") long messageId, Message message) {
+		message.setId(messageId);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void deleteMesssage(@PathParam("messageId") long messageId) {
+		 messageService.removeMessage(messageId); 
 	}
 
 }
--- src/main/java/com/aditya/restlearn/service/MessageService.java
+++ src/main/java/com/aditya/restlearn/service/MessageService.java
@@ -1,6 +1,7 @@
 package com.aditya.restlearn.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -14,12 +15,40 @@ public class MessageService {
 	public MessageService() {
 		messages.put(1L, new Message(1,"Hello world","koushik"));
 		messages.put(2L, new Message(2, "Hello jersey","aditya"));
+		messages.put(3L, new Message(2, "Hello jersey1","aditya1"));
+		messages.put(4L, new Message(2, "Hello jersey2","aditya2"));
+		messages.put(5L, new Message(2, "Hello jersey3","aditya3"));
+		messages.put(6L, new Message(2, "Hello jersey4","aditya4"));
+		messages.put(7L, new Message(2, "Hello jersey5","aditya5"));
+		messages.put(8L, new Message(2, "Hello jersey6","aditya6"));
+		messages.put(9L, new Message(2, "Hello jersey7","aditya7"));
+
 	}
 	
 	public List<Message> getAllMessage(){
 		return new ArrayList<Message>(messages.values());
 	}
 	
+	
+	public List<Message> getAllMesssagesForYear(int year){
+		List<Message> messagesForYear = new ArrayList<>();
+		Calendar cal = Calendar.getInstance();
+		for(Message message: messages.values()) {
+			cal.setTime(message.getCreated());
+			if(cal.get(Calendar.YEAR) == year) {
+				messagesForYear.add(message);
+			}
+		}
+		return messagesForYear;
+	}
+	
+	public List<Message> getAllMessagesPaginated(int start, int size) {
+		ArrayList<Message> list = new ArrayList<>(messages.values());
+		if(start + size > list.size()) return new ArrayList<Message>();
+		return list.subList(start, start + size);
+	}
+	
+
 	public Message getMessage(long id) {
 		if(messages.containsKey(id)) {
 			return messages.get(id);
